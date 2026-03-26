097844b53f640e988860a13e9151d4f1e43c4429
geethagagan01-cmd <geethagagan01@gmail.com>
2026-03-26 10:06:37 +0530

    Created bfs

---
+++ bfs.py
@@ -0,0 +1,29 @@
+from collections import deque
+
+def bfs(graph, start):
+    visited = set()
+    queue = deque()
+
+    visited.add(start)
+    queue.append(start)
+
+    while queue:
+        node = queue.popleft()
+        print(node, end=" ")
+
+        for neighbor in graph[node]:
+            if neighbor not in visited:
+                visited.add(neighbor)
+                queue.append(neighbor)
+
+# Example graph (Adjacency List)
+graph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'E'],
+    'D': ['B'],
+    'E': ['B', 'C']
+}
+
+# Run BFS
+bfs(graph, 'A')
