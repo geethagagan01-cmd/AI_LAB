09d13b03cada2b81801cc1c74aac94f7c1bc7a95
geethagagan01-cmd <geethagagan01@gmail.com>
2026-04-09 09:11:27 +0530

    Create dfs.py

---
+++ dfs.py
@@ -0,0 +1,27 @@
+def dfs(tree, node, visited=None):
+    if visited is None:
+        visited = []
+
+    visited.append(node)
+
+    for child in tree[node]:
+        if child not in visited:
+            dfs(tree, child, visited)
+
+    return visited
+
+
+# Example tree
+tree = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': [],
+    'F': []
+}
+
+# Execution
+print("DFS Traversal:", dfs(tree, 'A'))
+from collections import deque
+
